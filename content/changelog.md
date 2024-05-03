d4cea015c20456d205ab166a48f02c8c94bef6b7
Robert Yin <bobertoyin@gmail.com>
2024-05-03 18:21:13 -0500

    add changelog page

---
+++ content/changelog.md
@@ -0,0 +1,13 @@
+# Change Log
+
+## `v3.0.0`
+
+- Migrated from a static site generator ([Zola](https://www.getzola.org)) to a web server framework ([Axum](https://crates.io/crates/axum) + [Tera](https://keats.github.io/tera))
+- Upgraded to [Bulma v1](https://bulma.io)
+  - Enabled dark theme + theme toggling support!
+- Migrated icons from [Font Awesome Icons](https://fontawesome.com) to [Phosphor Icons](https://phosphoricons.com)
+- Replaced [Public Sans](https://fonts.google.com/specimen/Public+Sans) and [Source Code Pro](https://fonts.google.com/specimen/Source+Code+Pro) fonts with [Berkeley Mono](https://berkeleygraphics.com/typefaces/berkeley-mono)
+- Dropped reference to `CC BY NC SA 4.0` license
+- Added a `projects` page
+- Spruced up general styling inconsistencies/oddities
+- Updated information on the home page
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ use tera::{Context, Tera};
 use tokio::{fs::File, io::AsyncReadExt, net::TcpListener};
 use tower_http::services::ServeDir;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 enum AppError {
     Template(tera::Error),
     Io(std::io::Error),
@@ -65,6 +67,11 @@ fn parse_markdown(content: &str) -> Result<String, Message> {
     )
 }
 
+fn render_template(tera: &Tera, name: &str, context: &mut Context) -> Result<String, tera::Error> {
+    context.insert("version", VERSION);
+    tera.render(name, context)
+}
+
 async fn index(State(tera): State<Arc<Tera>>) -> Result<Html<String>, AppError> {
     let mut context = Context::new();
     let mut content = String::new();
@@ -75,19 +82,33 @@ async fn index(State(tera): State<Arc<Tera>>) -> Result<Html<String>, AppError> 
     context.insert("current_url", "/");
 
     context.insert("content", &parse_markdown(&content)?);
-    Ok(Html(tera.render("index.html", &context)?))
+    context.insert("basic_title", "home");
+    Ok(Html(render_template(&tera, "basic.html", &mut context)?))
 }
 
 async fn blog(State(tera): State<Arc<Tera>>) -> Result<Html<String>, AppError> {
     let mut context = Context::new();
     context.insert("current_url", "/blog");
-    Ok(Html(tera.render("blog.html", &context)?))
+    Ok(Html(render_template(&tera, "blog.html", &mut context)?))
 }
 
 async fn projects(State(tera): State<Arc<Tera>>) -> Result<Html<String>, AppError> {
     let mut context = Context::new();
     context.insert("current_url", "/projects");
-    Ok(Html(tera.render("projects.html", &context)?))
+    Ok(Html(render_template(&tera, "projects.html", &mut context)?))
+}
+
+async fn changelog(State(tera): State<Arc<Tera>>) -> Result<Html<String>, AppError> {
+    let mut context = Context::new();
+    let mut content = String::new();
+    File::open("content/changelog.md")
+        .await?
+        .read_to_string(&mut content)
+        .await?;
+
+    context.insert("content", &parse_markdown(&content)?);
+    context.insert("basic_title", "changelog");
+    Ok(Html(render_template(&tera, "basic.html", &mut context)?))
 }
 
 #[tokio::main]
@@ -97,6 +118,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .route("/", get(index))
         .route("/blog", get(blog))
         .route("/projects", get(projects))
+        .route("/changelog", get(changelog))
         .with_state(Arc::new(tera))
         .nest_service("/static", ServeDir::new("static"));
     let listener = TcpListener::bind("0.0.0.0:3000").await?;
